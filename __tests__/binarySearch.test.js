7007e06503cf8c6a88e58c5ce220250968aa372d
tim0git <tim.doolan88@icloud.com>
2020-05-13 09:42:05 +0100

    first commit

--- __tests__/binarySearch.test.js
+++ __tests__/binarySearch.test.js
@@ -1,7 +1,39 @@
-const { binarySearch } = require('../binarySearch')
+const { binarySearch } = require("../binarySearch");
 
-describe('binarySearch()', () => {
-  it('', () => {
-    // first test here
-  })
-})
+describe("binarySearch()", () => {
+  test("when passed an array of three consecutive numbers return index 1", () => {
+    const input = [1, 2, 3];
+    const searchParamter = 2;
+    expect(binarySearch(input, searchParamter)).toBe(1);
+  });
+  test("can find element in rhs of array", () => {
+    const input = [1, 2, 3, 4, 5];
+    const searchParamter = 4;
+    expect(binarySearch(input, searchParamter)).toBe(3);
+  });
+  test("can find element in lhs of array", () => {
+    const input = [1, 2, 3, 4, 5];
+    const searchParamter = 2;
+    expect(binarySearch(input, searchParamter)).toBe(1);
+  });
+  test("can find element in end rhs of array", () => {
+    const input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const searchParamter = 10;
+    expect(binarySearch(input, searchParamter)).toBe(9);
+  });
+  test("can find element in start lhs of array", () => {
+    const input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const searchParamter = 1;
+    expect(binarySearch(input, searchParamter)).toBe(0);
+  });
+  test("can find element in end rhs of array", () => {
+    const input = [1, 2, 3, 5, 6, 7, 8, 9, 10];
+    const searchParamter = 4;
+    expect(binarySearch(input, searchParamter)).toBe(-1);
+  });
+  test("can find element in end rhs of array", () => {
+    const input = ["a", "b", "c", "d", "e", "f", "g"];
+    const searchParamter = "f";
+    expect(binarySearch(input, searchParamter)).toBe(5);
+  });
+});
--- binarySearch.js
+++ binarySearch.js
@@ -1,6 +1,20 @@
 // Please do not change the name of this function
-const binarySearch = (arr) => {
+const binarySearch = (arr, searchParameter) => {
   // Your code here
-}
+  let L = 0; let R = arr.length - 1;
+  
+  while (L <= R) {
+    // loop through array, find the middle and compare it against searchParameter.
+    let middle = Math.floor((L + R) / 2);
+    // if searchParameter == middle return middle.
+    if (arr[middle] === searchParameter) return middle; //condition 1
+    // else if middle is < searchParameter set left hand side to be middle +1. (rhs only)
+    // run loop again ^^ see conditon 1
+    else if (arr[middle] < searchParameter) L = middle + 1;
+    // esle set right to be middle -1  
+    else R = middle - 1;
+  }
+  return -1;
+};
 
-module.exports = { binarySearch }
+module.exports = { binarySearch };
